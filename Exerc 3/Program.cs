34bb2ceeec156326df283cc059b17427716d363d
Caio Sousa <126969871+caio-sousa10@users.noreply.github.com>
2023-04-27 20:22:31 -0300

    Add files via upload

---
+++ Exerc 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3) Arquivo "datas" extrair somente o ano e mostrar no console​
+
+// instanciando objeto reader
+TextReader reader = new StreamReader(@"C:\aula 6\datas.txt");
+
+// criando variavel linha
+String linha = reader.ReadLine();
+
+// loop para ler o arquivo inteiro linha por linha
+while (linha!= null)
+{
+    int posicao_barra1 = linha.IndexOf('/');
+    string linha_printada = linha.Substring(posicao_barra1 + 4, 4);
+    linha = reader.ReadLine() ;
+    Console.WriteLine(linha_printada);
+}
---
+++ Exerc 4/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4) Arquivo "marcas_carros" extrair somente as marcas e apresentar no console​
+
+// instanciando objeto reader
+TextReader reader = new StreamReader(@"C:\aula 6\marcas_carros.txt");
+
+// criando variavel linha
+String linha = reader.ReadLine();
+
+// loop para ler o arquivo inteiro linha por linha
+while (linha != null)
+{
+    string[] partes_divididas= linha.Split('\t');
+    string marca = partes_divididas[0].Trim();
+    linha = reader.ReadLine();
+    Console.WriteLine(marca);
+}
