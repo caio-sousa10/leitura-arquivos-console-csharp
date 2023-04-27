5933ce1485885248a219c29b8ebf5aa47f71d359
Caio Sousa <126969871+caio-sousa10@users.noreply.github.com>
2023-04-27 20:21:28 -0300

    Add files via upload

---
+++ Exerc 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// 2) Arquivo "nomes_telefones" para cada nome de pessoa, verificar os maiores de 12 caracteres e
+// apresentar na tela os mesmos em UpperCase
+
+// instanciando objeto reader
+TextReader reader = new StreamReader(@"C:\aula 6\nomes_telefones.txt");
+
+// criando variavel linha
+String linha = reader.ReadLine();
+
+// loop para ler o arquivo inteiro linha por linha
+while (linha != null)
+{
+    string[] partes_divididas = linha.Split(' ');
+    string nome = partes_divididas[0].Trim();
+    nome += " " + partes_divididas[1];
+    linha = reader.ReadLine();
+    if (nome.Length > 12)
+    {
+        Console.WriteLine(nome.ToUpper());
+    }
+}
---
+++ exerc_aula_22.03/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1) Arquivo "nomes_telefones" para cada número de telefone, retirar os parenteses e traços e apresentar no console​
+
+// instanciando objeto reader
+TextReader reader = new StreamReader(@"C:\aula 6\nomes_telefones.txt");
+
+// criando variavel linha
+String linha = reader.ReadLine();
+
+// loop para ler o arquivo inteiro linha por linha
+while (linha != null)
+{
+    int posicao_parenteses1 = linha.IndexOf('(');
+    int posicao_parenteses2 = linha.IndexOf(')');
+    int posicao_traco = linha.IndexOf('-');
+    string linha_printada = linha.Substring(posicao_parenteses1 + 1, 2);
+    string linha_printada_meio = linha.Substring(posicao_parenteses2 + 1, 5);
+    string linha_printada_final = linha.Substring(posicao_traco + 1, 4);
+    Console.WriteLine(linha);
+    linha = reader.ReadLine();
+    Console.WriteLine(linha_printada + linha_printada_meio + linha_printada_final);
+}
